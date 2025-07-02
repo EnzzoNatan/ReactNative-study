b84727039f875c74799bce223cf5c587bf42fc9f
EnzzoNatan <enzzoferrazz1909@gmail.com>
2025-07-02 04:05:49 -0300

    hidden functions

--- CommunJs-modules/src/main.js
+++ CommunJs-modules/src/main.js
@@ -5,12 +5,13 @@ async function main(){
     console.log("Hello world")
     p.getFullName("123", "produto")
     // p.getFullName("234", "lanterna") CTRL + K+ C COMENTA TUDO
-    // p.getFullName("788", "geladeira")
-    // p.getFullName("345", "xbox")
+     p.getFullName("788", "geladeira")
+     p.getFullName("345", "xbox")
     // p.getFullName("564", "notebook")
     // p.getProductLabel("geladeira")
 
     console.log(config.production)
+    p.productType.version
 
 
 }
--- CommunJs-modules/src/services/products.js
+++ CommunJs-modules/src/services/products.js
@@ -1,10 +1,20 @@
 //TODAS AS FUNÇÕES QUE LIDAM COM PRODUTO
 
+const productType = {
+    version: "Digital",
+    tax: "x1"
+}
+
 async function getFullName(codeId, prodcutName) {
     console.log("\n")
     console.log( "Produto: " + codeId + " -- " + prodcutName)
+    await quebraDeLinha()
 }
 
+//Hidden function ou função escondida, pois só é lida dentro de outra função
+async function quebraDeLinha(){
+    console.log("\n")
+}
 
 async function getProductLabel(prodcutName){
     return console.log("Produto: " + prodcutName)
@@ -15,6 +25,7 @@ async function getProductLabel(prodcutName){
 //Exportando as funções 
 module.exports = {
     getFullName, // O export pode exportar mais de uma função
-    getProductLabel
+    getProductLabel,
+    productType,
 }
 
