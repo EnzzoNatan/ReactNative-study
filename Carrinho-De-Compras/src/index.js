353d5c374614158c9fc21c307445fdd37c256fef
EnzzoNatan <enzzoferrazz1909@gmail.com>
2025-07-04 04:48:33 -0300

    Add novas funções no carrinho

--- Carrinho-De-Compras/src/index.js
+++ Carrinho-De-Compras/src/index.js
@@ -7,14 +7,30 @@ const listaDeDesejo = []
 
 console.log("Bem-vindo ao carrinho de compras!");
 
+console.log("\n")
+
 const item1 = await criarItem("Peça de moto", 50.00, 3)
 const item2 = await criarItem("Peça do carro", 80.00, 2)
+const item3 = await criarItem("Motor", 1800.00, 5)
 
+//ADICIONANDO ITEM AO CARRINHO
 await servicoCarrinho.addItem(carrinho, item1)
 await servicoCarrinho.addItem(carrinho, item2)
+await servicoCarrinho.addItem(carrinho, item3)
+await servicoCarrinho.verCarrinho(carrinho)
 
 
-console.log("Carrinho total é:")
+//DELETANDO ITEM DO CARRINHO
+// await servicoCarrinho.deleteItem(carrinho, item1.nome)
+// await servicoCarrinho.deleteItem(carrinho, item2.nome)
+
+
+
 await servicoCarrinho.calcularTotal(carrinho)
+
+
+
+
+
 
 //console.log("Total dos itens do carrinho: ","\n",item2.subtotal(),"\n",item1.subtotal())
--- Carrinho-De-Compras/src/services/carrinho.js
+++ Carrinho-De-Compras/src/services/carrinho.js
@@ -8,18 +8,26 @@ async function addItem(usuarioCarrinho, item){
 
 //calcular o valor total✅
 async function calcularTotal(usuarioCarrinho){
+    console.log("VALOR TOTAL DO CARRINHO: ")
     const resultado = usuarioCarrinho.reduce((total,item) => total + item.subtotal(),0)
-    console.log(resultado)
+    console.log(`Total R$ ${resultado}`)
 }
 
-//deletar item do carrinho
+//deletar item do carrinho✅
 async function deleteItem(usuarioCarrinho, nome){
-    const indnex = usuarioCarrinho.findIndex((item) => item.nome === nome)
+    const index = usuarioCarrinho.findIndex((item) => item.nome === nome)
 
     if(index !== -1){
         usuarioCarrinho.splice(index, 1)
     }
 
+}
+
+async function verCarrinho(usuarioCarrinho){
+    console.log("LISTA DE COMPRAS: ")
+    usuarioCarrinho.forEach((item, index) => {
+        console.log(`${index + 1}. ${item.nome} - R$ ${item.preco} x ${item.quantidade} = R$ ${item.subtotal()}`)
+    })
 }
 
 //remover item do carrinho
@@ -33,5 +41,6 @@ export{
     addItem,
     deleteItem,
     removeItem,
-    calcularTotal
+    calcularTotal,
+    verCarrinho
 }
