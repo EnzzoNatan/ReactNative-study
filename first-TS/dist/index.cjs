f3fd798da5c84df5186d2afb7234db6914b8ea7d
EnzzoNatan <enzzoferrazz1909@gmail.com>
2025-07-20 04:14:40 -0300

    primeira consiguração do typescript

---
+++ first-TS/dist/index.cjs
@@ -0,0 +1,8 @@
+"use strict";
+
+// src/index.ts
+function main() {
+  let meuEmail = "Enzzo123@gmail.com";
+  console.log(meuEmail);
+}
+main();
---
+++ first-TS/src/index.ts
@@ -0,0 +1,7 @@
+function main(){
+    let meuEmail: string = "Enzzo123@gmail.com"
+
+    console.log(meuEmail);
+}
+
+main()
